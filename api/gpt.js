2366e456fc578dcbaa209526027c6da2d9078587
geralm <estebanlm@estudiantec.cr>
2023-11-05 11:49:01 -0600

    add some import un chatgptapi

--- api/gpt.js
+++ api/gpt.js
@@ -1,7 +1,8 @@
 
 const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '../config/.env') });
-import {fs} from 'fs';
+const fs = require('fs');
+
 
 const OpenAI = require("openai");
 
